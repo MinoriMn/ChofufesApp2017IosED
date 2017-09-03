edfcda06aa3cf57db5549a25cff144c4682f8c87
mohupero <core.filemode>
2017-09-03 21:12:59 +0900

    Initial Commit

---
+++ 67thChofufesApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  67thChofufesApp
+//
+//  Created by 松田尚也 on 2017/09/03.
+//  Copyright © 2017年 67thChofufes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
